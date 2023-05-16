b59123b4ec6a01ae80f5dc2dd362a989e0b4bcbe
Carollina Moraes <carolmoraes333@gmail.com>
2023-05-16 16:49:27 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("A aplicação está se conectando ao banco de dados")
+console.log("Teste")
+console.log("Enviando para o GitHub")
+
 
